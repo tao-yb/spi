7c5f2940c505ac7daf6439e6ec076fefe3384f31
yongbo.tao <yongbo.tao@qingteng.cn>
2023-01-16 18:52:14 +0800

    删除无用文件，测试compiler 加载的class 运行效果

--- src/main/java/com/spi/Main.java
+++ src/main/java/com/spi/Main.java
@@ -1,14 +1,34 @@
 package com.spi;
 
-import com.spi.compile.Compiler;
 import com.spi.common.ExtensionLoader;
+import com.spi.compile.Compiler;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+
+import java.lang.reflect.Method;
 
 @Slf4j
 public class Main {
 
+    @SneakyThrows
     public static void main(String[] args) {
         final Compiler compiler = ExtensionLoader.getExtensionLoader(Compiler.class).getDefaultExtension();
+        final Class<?> testClass = compiler.compile(generateCode(),
+                Thread.currentThread().getContextClassLoader());
+        final Method[] declaredMethods = testClass.getDeclaredMethods();
+        for (Method declaredMethod : declaredMethods) {
+            declaredMethod.invoke(testClass.newInstance(),"abc");
+        }
         log.info(compiler.toString());
+    }
+
+    static String generateCode() {
+        return "package com.spi;\n" +
+                "\n" +
+                "public class Test {\n" +
+                "    public void print(String arg) {\n" +
+                "        System.out.println(\"com.spi.Test print msg:\" + arg);\n" +
+                "    }\n" +
+                "}\n";
     }
 }
