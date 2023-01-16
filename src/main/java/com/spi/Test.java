e9180695b6612d522da9fe1b7e6e065e6369e9c7
yongbo.tao <yongbo.tao@qingteng.cn>
2023-01-16 18:44:18 +0800

    测试complier 加载Code

---
+++ src/main/java/com/spi/Test.java
@@ -0,0 +1,10 @@
+package com.spi;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Test {
+    public void print(String arg) {
+        log.info("com.spi.Test print msg:{}", arg);
+    }
+}
--- src/main/java/com/spi/common/DisableInject.java
+++
@@ -1,9 +0,0 @@
-package com.spi.common;
-
-import java.lang.annotation.*;
-
-@Documented
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE, ElementType.METHOD})
-public @interface DisableInject {
-}
